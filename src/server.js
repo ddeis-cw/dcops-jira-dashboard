95d98c5851b83c21ad5cbb475ae4cd1116be3a7a
Devon Deis <ddeis@coreweave.com>
2026-04-28 15:54:47 -0700

    Trends: fix strftime on timezone-offset dates with SUBSTR

--- src/server.js
+++ src/server.js
@@ -232,7 +232,7 @@ app.get('/api/trends', (req, res) => {
           e.site
         ), 1, 6) AS site,
         t.project,
-        strftime(?, t.created_at) AS bucket,
+        strftime(?, SUBSTR(t.created_at, 1, 19)) AS bucket,
         COUNT(*) AS n
       FROM tickets t
       LEFT JOIN employees e ON t.assignee = e.name
